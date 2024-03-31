e5f3c8c1f5b9f86d7dec463fad61fd91e4db4e84
Deb Kalyan <Deb.Kalyan@markoknow.com>
2024-03-31 20:23:48 +0530

    Updated The Resume Link

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
         <SectionText>
         Spiritually-grounded web developer passionate about crafting meaningful online experiences with a touch of transcendence
         </SectionText>
-        <Button onClick={() => (window.location = 'https://drive.google.com/file/d/1vu9AHZ-KtRLP8iKh2G9DUgAxJkIh3mYM/view?usp=sharing')}>
+        <Button onClick={() => (window.location = 'https://drive.google.com/file/d/1Of4hwbx2lV5eN_5jQx9uqkJ2nsXM12aA/view?usp=sharing')}>
           Resume
         </Button>
 
